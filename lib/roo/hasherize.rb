a92d5efc363d1aec58aff51f128988e82dc9ac97
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2015-06-05 17:41:52 +0300

    Up

--- lib/roo/hasherize.rb
+++ lib/roo/hasherize.rb
@@ -1,11 +1,13 @@
 require 'roo/hasherize/version'
+require 'roo/hasherize/worksheet'
+require 'roo/hasherize/parser'
 
 module Roo
   # Main gem module.
   # Also acts as a facade / public API
   module Hasherize
     def self.call(target, options = {})
-      Roo::Hasherize::Parser.call(target, options)
+      Parser.call(target, options)
     end
   end
 end
